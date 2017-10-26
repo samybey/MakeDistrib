278b1bdd6a18229c40505a4f852b5643576432d5
Nicolas Follet <follet.nico@gmail.com>
2017-10-26 14:33:32 +0200

    ajout du split des dependences

--- main.cpp
+++ main.cpp
@@ -6,25 +6,52 @@
 using namespace std;
 
 int main (int argc, char* argv[]) {
-     
-     string makefile = parserfichier(argv[1]);
-     smatch match;
-     
-     try{
-	  regex reg ("(?:([\\w]+(?:.[\\w]+)?):(?: ([\\w. ]+))?\\n\\t([\\w. -_]+)(?:\\n|\\Z)+)", regex_constants::ECMAScript);
-	  while (regex_search (makefile, match, reg)) {
-	       for (auto x:match) 
-		    cout << x << " ";
-	       cout << endl;
-	       makefile = match.suffix().str();
-	  }
-     } catch (const regex_error &e) {
-	  cout << "error :" << "\n";
-	  cout << e.what();
-	  if (e.code() == regex_constants::error_brack) {
-	       cout << "The code was error_brack\n";
-	  }
-     }
-     return 0;
-     
+
+	string makefile = parserfichier(argv[1]);
+	smatch match;
+
+	try{
+		regex reg ("(?:([\\w]+(?:.[\\w]+)?):(?: ([\\w. ]+))?\\n\\t([\\w. -_]+)(?:\\n|$)+)", regex_constants::ECMAScript);
+		while (regex_search (makefile, match, reg)) {
+			int i = 0;
+			string dependence;
+			string reste;
+			for (auto x:match) {
+				i++;
+				switch (i) {
+				case 1:
+					break;
+				case 2:
+					cout << "destination : " << x << "\n";
+					break;
+				case 3:
+					cout << "dépendences : \n";
+					reste = x.str();
+					dependence = reste.substr(0, reste.find(" "));
+					while (dependence != reste) {
+						cout << dependence << "\n";
+						reste = reste.substr(reste.find(" ") + 1, reste.size()-1);
+						dependence = reste.substr(0, reste.find(" "));
+
+					}
+					 cout << reste << "\n";
+					break;
+				case 4:
+					cout << "commande : " << x << "\n";
+				default:
+					break;
+				}
+			}
+			cout << endl;
+			makefile = match.suffix().str();
+		}
+	} catch (const regex_error &e) {
+		cout << "error :" << "\n";
+		cout << e.what();
+		if (e.code() == regex_constants::error_brack) {
+			cout << "The code was error_brack\n";
+		}
+	}
+	return 0;
+
 } 
