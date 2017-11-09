d967d0ebc4ee61b6a362d36c9b5621c39ec5b685
Cyril Gaunet <cyril.gaunet@ensimag.grenoble-inp.fr>
2017-11-09 15:21:40 +0100

    Plus de pb char*

--- Charm/StringNode.C
+++ Charm/StringNode.C
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-StringNode::stringNode(std::string name,
+StringNode::StringNode(std::string name,
 	std::vector<std::string> dependencesVector,
 	std::string command) :
 	m_name(name), m_dependencesVector(dependencesVector), m_command(command) {
 }
 
 bool StringNode::deleteDependence(string dependence) {
-	vector<string>::iterator it = find(m_dependencesVector.begin(),
+  vector<string>::iterator it = std::find(m_dependencesVector.begin(),
 		m_dependencesVector.end(), dependence);
 
 	if (it == m_dependencesVector.end())
@@ -21,11 +21,11 @@ bool StringNode::deleteDependence(string dependence) {
 }
 
 void StringNode::displayNode() {
-	cout << "Target : \n\t" << m_name << "\n";
-	cout << "Dependences : \n";
+	std::cout << "Target : \n\t" << m_name << "\n";
+	std::cout << "Dependences : \n";
 	for (auto i : m_dependencesVector) {
-		cout << "\t" << i << "\n";
+		std::cout << "\t" << i << "\n";
 	}
-	cout << "Command : \n\t" << m_command << "\n\n";
+	std::cout << "Command : \n\t" << m_command << "\n\n";
 }
 #include "StringNode.def.h"
