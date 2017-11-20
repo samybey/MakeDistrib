8c4ac05aaf356448ae51b388a1ac49e2c8ecb6b5
Thomas Trompette <ttrompette@fnancy>
2017-11-20 19:28:31 +0100

    script temps changé

--- temps.sh
+++ temps.sh
@@ -1,6 +1,9 @@
 cd test/makefiles/premier
-for i in `seq 1 3 40`;
+for i in `seq 8 3 25`;
 do
     touch time$i.log
-    time ./../../../charmrun +p$i ++remote-shell oarsh ./../../../Main Makefile +isomalloc_sync +traceroot ../../../log > time$i.log
+    touch timesimple$i.txt
+    time -o timesimple$i.txt ./../../../charmrun +p$i ++remote-shell oarsh ./../../../Main Makefile +isomalloc_sync +traceroot ../../../log
+    ../../../log/Main.sts > time$i.log
+    rm list*
 done
