d7e9e465166d776d007cca849e012f9082cf4ab4
Nicolas Follet <nfollet@graphene-105.nancy.grid5000.fr>
2017-11-23 16:49:13 +0100

    modifiations du Makefile de premier100 pour qu'il marche

--- test/premier100/gen_makefile.sh
+++ test/premier100/gen_makefile.sh
@@ -1,14 +1,13 @@
 echo 'list1.txt:' > MakefileTemp
 echo '\t./premier 2 `echo 100*10000000/100-1 |bc` > list1.txt' >> MakefileTemp
-echo -n 'list.txt:\t' > Makefile
+echo -n 'list.txt:' > Makefile
 echo -n ' list1.txt' > dependences
 echo -n '\tcp list1.txt list.txt' > command
 
 for i in `seq 2 1 100`;
 do
-    echo '' >> MakefileTemp
     echo 'list'$i'.txt:' >> MakefileTemp
-    echo '\t./premier `echo ('$i'-1)*100*10000000/100 |bc` `echo '$i'*100*10000000/100-1 |bc` > list'$i'.txt' >> MakefileTemp
+    echo '\t./premier `echo '$(($i-1))'*100*10000000/100 |bc` `echo '$i'*100*10000000/100-1 |bc` > list'$i'.txt' >> MakefileTemp
     echo -n ' list'$i'.txt' >> dependences
     echo -n '; cat list'$i'.txt >> list.txt' >> command
 done
